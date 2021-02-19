f416d5bec455cce5230adc583b225166bf1e23ac
sagarmc789 <75300834+sagarmc789@users.noreply.github.com>
2021-02-19 14:01:59 +0530

    Add files via upload

---
+++ Bob.js
@@ -0,0 +1,23 @@
+class Bob {
+    constructor(x, y) {
+      var options = {
+          isStatic:false,
+        'restitution':0.3,
+          'friction':0.5,
+          'density':1.2
+          
+      }
+      this.body = Bodies.circle(x, y, 25,options);
+    
+      World.add(world, this.body);
+    }
+    display(){
+        var pos =this.body.position;
+        ellipseMode(RADIUS)
+        fill("pink");
+        ellipse(pos.x, pos.y, 25,25);
+      
+
+
+    }
+  };
--- README.md
+++ README.md
@@ -1,1 +1,2 @@
-# abc
+# MatterJSBoilerPlate
+MatterJSBoilerPlate
